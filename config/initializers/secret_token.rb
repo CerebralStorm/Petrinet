36255d7367f78455e9750d6eb8e50f8a6f919dbc
Cody Tanner <cerebralstorn@gmail.com>
2012-11-15 18:18:17 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Petrinet::Application.config.secret_token = 'f26b7486b848c4192cf496929c9abadbbf09ec30c07bd2c2f0cdad3cf4eea33dd9d0ddae589dbbed23c51f967605ec6c170b7894fdaa9af1a2d38ef6b6333d19'
